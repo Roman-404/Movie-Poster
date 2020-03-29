747e5ac8705fceceec927e9da0e491fcab44fd91
Roman Belotsvetov <rbelotsvetov88@yandex.ru>
2020-03-29 19:16:26 +0300

    ...

--- src/components/loading/loading.js
+++ src/components/loading/loading.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import LinearProgress from '@material-ui/core/LinearProgress';
+import { withStyles } from '@material-ui/core/styles';
 import './loading.css';
+
+const LoadLinearProgress = withStyles({
+    colorPrimary: {
+      height: 7,
+      backgroundColor: '#b2dfdb',
+    },
+    barColorPrimary: {
+      backgroundColor: '#0a97a1',
+    },
+  })(LinearProgress);
 
 const Loading = ({ loading }) => {
     return (
         <div className = {loading ? 'loading' : 'loaded'}>
-            <LinearProgress color='secondary'/>
+            <LoadLinearProgress/>
         </div>
     )
 }
