d19fdf257255e24752280e318d2ccfdf703eb393
Roman Belotsvetov <rbelotsvetov88@yandex.ru>
2020-04-04 21:03:07 +0300

    ...

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import CatchError from './components/error-catch/error-catch';
 import { BrowserRouter as Router } from 'react-router-dom';
 import store from './store';
+// import './static/theme/bootstrap.css';
 
 ReactDOM.render(
     <Provider store={store}>
