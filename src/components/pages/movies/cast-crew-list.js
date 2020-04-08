64e25d071b338444f5bc4a91062f6d7b5b251787
Roman Belotsvetov <rbelotsvetov88@yandex.ru>
2020-04-09 01:02:17 +0300

    ...

--- src/components/pages/movies/cast-crew-list.js
+++ src/components/pages/movies/cast-crew-list.js
@@ -5,7 +5,7 @@ export default ({ person, url, role }) => {
     return (
         <div className='cast-crew' key={person.id}>
             <figure>
-                <img src={url} alt='None'/>
+                <img src={url} alt='None' onError={e => getBrokenImageIcon(e, person.gender)}/>
                 <figcaption>
                     <div className='cast-crew-name'>{person.name}</div>
                     <div className='cast-crew-role'>{role}</div>
@@ -13,4 +13,9 @@ export default ({ person, url, role }) => {
             </figure>
         </div>
     )
+}
+
+const getBrokenImageIcon = (event, gender) => {
+    event.target.style = 'width: 200px; height: 300px'
+    event.target.src = require(`../../../static/img/${gender ? gender === 2 ? 'male.png' : 'famale.png' : 'man.png'}`)
 }
