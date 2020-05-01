ed2e2c3428a6e0efcf78b2de99efe43994642953
Roman Belotsvetov <rbelotsvetov88@yandex.ru>
2020-05-01 23:17:02 +0300

    ...

--- src/components/header/burger-menu.js
+++ src/components/header/burger-menu.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 export default ({ page }) => {
     return (
         <div className='burger-menu'>
-            <Menu>
+            <Menu noOverlay>
                 <Link to={`/movies/${page ? `?page=${page}` : ''}`}>Movies</Link>
                 <Link to='/people/'>People</Link>
                 <Link to='#'>Login</Link>
